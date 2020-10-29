f17c5768e0d042f24e5bb697c0565cac6ae83727
Dmitry <dmitry@cojam.ru>
2020-10-29 21:04:50 +0500

    вёрстка футера

--- src/components/Master/MasterFooter/MasterFooter.tsx
+++ src/components/Master/MasterFooter/MasterFooter.tsx
@@ -3,20 +3,64 @@ import { Link } from 'react-router-dom';
 
 import { Container } from '../../common';
 
+import { ImageLogoFooter } from '../../../static/images';
+
 import './MasterFooter.scss';
 
 const MasterFooter: React.FC = () => {
     return (
         <footer className="app-footer">
             <Container>
+                <div className="app-footer__attribution">
+                    <div className="app-footer__attribution__logo">
+                        <a href="https://civsoc.net" target="_blank" rel="noreferrer noopener">
+                            <img src={ImageLogoFooter} alt="civsoc logo" />
+                        </a>
+                    </div>
+                    <div className="app-footer__attribution__text">
+                        <a href="https://civsoc.net/mgrob/" target="_blank" rel="noreferrer noopener">
+                            <span>Молодёжное</span> <span>Гражданское</span> <span>Общество</span>
+                        </a>
+                    </div>
+                </div>
                 <div className="app-footer__links">
-                    <a href="https://civsoc.net/mgrob/" target="_blank" rel="noopener noreferrer">
-                        Проект Молодёжного Гражданского Общества
-                    </a>{' '}
-                    &middot;{' '}
-                    <Link to="/about" style={{ whiteSpace: 'nowrap' }}>
-                        О проекте
-                    </Link>
+                    <div className="app-footer__links__section">
+                        <p>Соцсети</p>
+                        <ul>
+                            <li>
+                                <a href="https://t.me" target="_blank" rel="noreferrer noopener">
+                                    telegram
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/studentfract" target="_blank" rel="noreferrer noopener">
+                                    twitter
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="app-footer__links__section">
+                        <p>В образовательные организации</p>
+                        <ul>
+                            <li>
+                                <Link to="/edu">раз</Link>
+                            </li>
+                            <li>
+                                <Link to="/edu">два много текста</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="app-footer__links__section">
+                        <p>В МВД</p>
+                        <ul>
+                            <li>
+                                <Link to="/police">раз </Link>
+                            </li>
+                            <li>
+                                <Link to="/police">два средне текста</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </Container>
         </footer>
---
+++ src/static/images/index.ts
@@ -0,0 +1,3 @@
+import ImageLogoFooter from './civsoc-logo-footer.svg';
+
+export { ImageLogoFooter };
